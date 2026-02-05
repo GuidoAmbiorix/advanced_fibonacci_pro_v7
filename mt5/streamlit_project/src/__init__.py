d39bb8a6e3607423dfefb3fca242826d0c245a31
gamparo <gamparo@rehabilitacion.org.do>
2026-02-05 10:47:23 -0400

    cleaning v1

--- mt5/streamlit_project/src/__init__.py
+++
@@ -1,9 +0,0 @@
-from .connector_wrapper import MT5Connector
-from .data_engine import DataEngine
-from .data_engine_cached import CachedDataEngine
-from .types import Trade
-from .patterns import PatternGeneric
-from .analytics import PerformanceAnalytics
-from .database import TradingDatabase, get_database
-from .config import config
-from .logger import get_logger, initialize_logging, shutdown_logging
